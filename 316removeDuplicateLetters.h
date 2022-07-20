5c196a529cd8a1e22e160358589ea72d1ec2bc3a
skyelves <1600012805@pku.edu.cn>
2022-07-20 18:30:36 -0400

    problem 316

---
+++ 316removeDuplicateLetters.h
@@ -0,0 +1,47 @@
+//
+// Created by 王柯 on 7/20/22.
+//
+
+#ifndef LEETCODE_316REMOVEDUPLICATELETTERS_H
+#define LEETCODE_316REMOVEDUPLICATELETTERS_H
+
+#include "solution.h"
+
+class RemoveDuplicateLetters : public solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string res = "";
+        set<char> flag;
+        int len = s.size();
+        for (int i = 0; i < len; ++i) {
+            int res_len = res.size();
+            if (res_len > 0) {
+                if (res.find(s[i]) != string::npos)
+                    continue; // exist
+                for (int j = res_len - 1; j >= 0; --j) {
+                    if (res[j] > s[i] && s.find(res[j], i) != string::npos) {
+                        // delete the letter
+                        res.pop_back();
+                    } else {
+                        break;
+                    }
+                }
+                res += s[i];
+            } else {
+                res += s[i];
+            }
+        }
+        return res;
+    }
+
+    void check() {
+        string s = "abacb"; // abc
+//        string s = "cbacdcbc"; //acdb
+//        string s = "bcab"; // bca
+//        string s = "bcabc"; // abc
+        string res = removeDuplicateLetters(s);
+        cout << res << endl;
+    }
+};
+
+#endif //LEETCODE_316REMOVEDUPLICATELETTERS_H
--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include "1_twoSum.h"
 #include "2_addTwoNumbers.h"
 #include "1021_removeOutermostParentheses.h"
+#include "2166_designBitset.h"
+#include "316removeDuplicateLetters.h"
 
-solution *sol = new RemoveOuterParentheses;
+solution *sol = new RemoveDuplicateLetters;
 
 int main() {
     sol->check();
