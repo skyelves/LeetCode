b742d04c75e7529b76b36583d5c27bbdf350d9cb
skyelves <1600012805@pku.edu.cn>
2022-10-02 17:55:43 -0400

    problem 56 O(nlogn)

--- 56_merge.h
+++ 56_merge.h
@@ -9,43 +9,61 @@
 
 class Merge : public solution {
 public:
+//    // O(N*range)
+//    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+//        int myi[10005] = {0};
+//        for (int i = 0; i < 10005; ++i) {
+//            myi[i] = -1;
+//        }
+//        vector<vector<int>> res;
+//        for (int i = 0; i < intervals.size(); ++i) {
+//            int start = intervals[i][0], end = intervals[i][1];
+//            int s = myi[start], e = myi[end];
+//            if (s == -1) {
+//                s = start;
+//                myi[start] = start;
+//            }
+//            if (s != e) {
+//                for (int j = end + 1; j < 10005; ++j) {
+//                    if (myi[j] == e && myi[j] != -1) {
+//                        myi[j] = s;
+//                    }
+//                }
+//                for (int j = start; j <= end; ++j) {
+//                    myi[j] = s;
+//                }
+//            }
+//        }
+//        int start = -1;
+//        for (int i = 0; i < 10005; ++i) {
+//            if (start == -1) {
+//                if (myi[i] == i) {
+//                    start = i;
+//                }
+//            } else {
+//                if (myi[i] != start) {
+//                    res.push_back({start, i - 1});
+//                    start = myi[i];
+//                }
+//            }
+//        }
+//        return res;
+//    }
     vector<vector<int>> merge(vector<vector<int>> &intervals) {
-        int myi[10005] = {0};
-        for (int i = 0; i < 10005; ++i) {
-            myi[i] = -1;
-        }
         vector<vector<int>> res;
-        for (int i = 0; i < intervals.size(); ++i) {
-            int start = intervals[i][0], end = intervals[i][1];
-            int s = myi[start], e = myi[end];
-            if (s == -1) {
-                s = start;
-                myi[start] = start;
-            }
-            if (s != e) {
-                for (int j = end + 1; j < 10005; ++j) {
-                    if (myi[j] == e && myi[j] != -1) {
-                        myi[j] = s;
-                    }
-                }
-                for (int j = start; j <= end; ++j) {
-                    myi[j] = s;
-                }
-            }
-        }
-        int start = -1;
-        for (int i = 0; i < 10005; ++i) {
-            if (start == -1) {
-                if (myi[i] == i) {
-                    start = i;
-                }
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int> &a, const vector<int> &b) { return a[0] < b[0]; });
+        int start = intervals[0][0], end = intervals[0][1];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] > end) {
+                res.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             } else {
-                if (myi[i] != start) {
-                    res.push_back({start, i - 1});
-                    start = myi[i];
-                }
+                end = max(intervals[i][1], end);
             }
         }
+        res.push_back({start, end});
         return res;
     }
 
