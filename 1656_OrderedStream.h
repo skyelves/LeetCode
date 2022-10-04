440f0b218fad9f875e911e2a1f144e06c7e464b9
skyelves <1600012805@pku.edu.cn>
2022-10-03 22:41:29 -0400

    problem 1656 without n

--- 1656_OrderedStream.h
+++ 1656_OrderedStream.h
@@ -9,29 +9,54 @@
 
 class OrderedStream {
 public:
-    int num;
-    bool *visited = NULL;
-    vector<string> str;
+//    int num;
+//    bool *visited = NULL;
+//    vector<string> str;
+//
+//    OrderedStream(int n) {
+//        num = 1;
+//        visited = new bool[n + 2];
+//        memset(visited, false, sizeof(bool) * (n + 2));
+//        str.resize(n + 2);
+//        visited[0] = true;
+//    }
+//
+//    vector<string> insert(int idKey, string value) {
+//        vector<string> res{};
+//        visited[idKey] = true;
+//        str[idKey] = value;
+//        if (idKey == num) {
+//            int i = idKey;
+//            while (visited[i]) {
+//                res.push_back(str[i]);
+//                i++;
+//                num++;
+//            }
+//        }
+//        return res;
+//    }
+    struct cmp {
+        bool operator()(const pair<int, string> &a, const pair<int, string> &b) {
+            return a.first < b.first;
+        }
+    };
+
+    priority_queue<pair<int, string>, vector<pair<int, string> >, cmp> pq;
+
+    int key;
 
     OrderedStream(int n) {
-        num = 1;
-        visited = new bool[n + 2];
-        memset(visited, false, sizeof(bool) * (n + 2));
-        str.resize(n + 2);
-        visited[0] = true;
+        key = 1;
     }
 
     vector<string> insert(int idKey, string value) {
-        vector<string> res{};
-        visited[idKey] = true;
-        str[idKey] = value;
-        if (idKey == num) {
-            int i = idKey;
-            while (visited[i]) {
-                res.push_back(str[i]);
-                i++;
-                num++;
-            }
+        vector<string> res;
+        pair<int, string> kv{idKey, value};
+        pq.push(kv);
+        while (pq.top().first == key) {
+            res.push_back(pq.top().second);
+            pq.pop();
+            key++;
         }
         return res;
     }
