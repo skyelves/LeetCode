07187d2e390be8ff7a3542107eddde15336e4ab4
skyelves <1600012805@pku.edu.cn>
2022-10-27 17:00:41 -0400

    problem 1244 tradeoff add/reset with topk

--- 1244_Leaderboard.h
+++ 1244_Leaderboard.h
@@ -9,7 +9,18 @@
 
 class Leaderboard {
 public:
-    map<int, int> IdScore;
+    struct comp {
+        bool operator()(const pair<int, int> &a, const pair<int, int> &b) const {
+            if (a.second != b.second) {
+                return a.second > b.second;
+            } else {
+                return a.first > b.first;
+            }
+        }
+    };
+
+    unordered_map<int, map<pair<int, int>, int>::iterator> IdScore;
+    map<pair<int, int>, int, comp> rever;
 
     Leaderboard() {
 
@@ -18,38 +29,71 @@ public:
     void addScore(int playerId, int score) {
         auto iter = IdScore.find(playerId);
         if (iter == IdScore.end()) {
-            IdScore.insert({playerId, score});
+            pair<int, int> tmp{playerId, score};
+            rever.insert({tmp, score});
+            IdScore[playerId] = rever.find(tmp);
         } else {
-            iter->second += score;
+            pair<int, int> tmp{playerId, (iter->second->second) + score};
+            rever.erase(iter->second);
+            rever.insert({tmp, tmp.second});
+            IdScore[playerId] = rever.find(tmp);
         }
     }
 
     int top(int K) {
-        priority_queue<int, vector<int>, greater<int> > pq;
-        auto iter = IdScore.begin();
-        for (int i = 0; i < K; ++i) {
-            pq.push(iter->second);
-            iter++;
-        }
-        while (iter != IdScore.end()) {
-            int tmp = pq.top();
-            if (tmp < iter->second) {
-                pq.pop();
-                pq.push(iter->second);
-            }
-            iter++;
-        }
         int res = 0;
-        while (!pq.empty()) {
-            res += pq.top();
-            pq.pop();
+        for (auto iter = rever.begin(); iter != rever.end() && K > 0; ++iter, --K) {
+            res += iter->second;
         }
         return res;
     }
 
     void reset(int playerId) {
-        IdScore.erase(playerId);
+        auto iter = IdScore.find(playerId);
+        rever.erase(iter->second);
+        IdScore.erase(iter);
     }
+//    map<int, int> IdScore;
+//
+//    Leaderboard() {
+//
+//    }
+//
+//    void addScore(int playerId, int score) {
+//        auto iter = IdScore.find(playerId);
+//        if (iter == IdScore.end()) {
+//            IdScore.insert({playerId, score});
+//        } else {
+//            iter->second += score;
+//        }
+//    }
+//
+//    int top(int K) {
+//        priority_queue<int, vector<int>, greater<int> > pq;
+//        auto iter = IdScore.begin();
+//        for (int i = 0; i < K; ++i) {
+//            pq.push(iter->second);
+//            iter++;
+//        }
+//        while (iter != IdScore.end()) {
+//            int tmp = pq.top();
+//            if (tmp < iter->second) {
+//                pq.pop();
+//                pq.push(iter->second);
+//            }
+//            iter++;
+//        }
+//        int res = 0;
+//        while (!pq.empty()) {
+//            res += pq.top();
+//            pq.pop();
+//        }
+//        return res;
+//    }
+//
+//    void reset(int playerId) {
+//        IdScore.erase(playerId);
+//    }
 };
 
 /**
