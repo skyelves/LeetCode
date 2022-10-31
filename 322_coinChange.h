b70742f0095270f780c28cee437ab95ef2961693
skyelves <1600012805@pku.edu.cn>
2022-10-30 22:42:51 -0400

    problem 322

--- 322_coinChange.h
+++ 322_coinChange.h
@@ -13,26 +13,43 @@ public:
         if (amount == 0)
             return 0;
         int len = coins.size();
-        int **dp = new int *[len];
-        for (int i = 0; i < len; ++i) {
-            dp[i] = new int[amount + 1];
-            for (int j = 0; j < amount + 1; ++j) {
-                dp[i][j] = INT_MAX - 1;
-            }
-        }
+        sort(coins.begin(), coins.end());
+        vector<int> dp(vector<int>(amount + 1, 1000000));
         for (int i = 0; i <= amount; i += coins[0]) {
-            dp[0][i] = i / coins[0];
+            dp[i] = i / coins[0];
         }
         for (int i = 1; i < len; ++i) {
-            for (int j = 0; j <= amount; ++j) {
-                if (coins[i] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - coins[i]] + 1);
+            for (int j = 1; j <= amount; ++j) {
+                if (coins[i] <= j) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
                 }
             }
         }
-        return dp[len - 1][amount] == INT_MAX - 1 ? -1 : dp[len - 1][amount];
+        return dp[amount] == 1000000 ? -1 : dp[amount];
+
+//        if (amount == 0)
+//            return 0;
+//        int len = coins.size();
+//        int **dp = new int *[len];
+//        for (int i = 0; i < len; ++i) {
+//            dp[i] = new int[amount + 1];
+//            for (int j = 0; j < amount + 1; ++j) {
+//                dp[i][j] = INT_MAX - 1;
+//            }
+//        }
+//        for (int i = 0; i <= amount; i += coins[0]) {
+//            dp[0][i] = i / coins[0];
+//        }
+//        for (int i = 1; i < len; ++i) {
+//            for (int j = 0; j <= amount; ++j) {
+//                if (coins[i] > j) {
+//                    dp[i][j] = dp[i - 1][j];
+//                } else {
+//                    dp[i][j] = min(dp[i - 1][j], dp[i][j - coins[i]] + 1);
+//                }
+//            }
+//        }
+//        return dp[len - 1][amount] == INT_MAX - 1 ? -1 : dp[len - 1][amount];
     }
 
     void check() {
