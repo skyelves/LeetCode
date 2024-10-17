f7f0767e2f7049c1e0511850385b4fb8a76c7748
skyelves <wangke971111@gmail.com>
2024-10-16 19:30:50 -0700

    updated solution

--- 772_BasicCalculatorIII.h
+++ 772_BasicCalculatorIII.h
@@ -9,98 +9,63 @@
 
 class BasicCaculatorIII : public solution {
 public:
-    string infix2suffix(string &s) {
-        string res;
-        stack<char> ss;
-        for (auto &c : s) {
-            if (c == ' ') continue;
-            if (c == '+' || c == '-') {
-                while(!ss.empty() && ss.top() != '(') {
-                    res.push_back(' ');
-                    res.push_back(ss.top());
-                    ss.pop();
-                }
-                res.push_back(' ');
-                ss.push(c);
-            } else if (c == '*' || c == '/') {
-                while(!ss.empty() && (ss.top() == '*' || ss.top() == '/')) {
-                    res.push_back(' ');
-                    res.push_back(ss.top());
-                    ss.pop();
-                }
-                res.push_back(' ');
-                ss.push(c);
-            } else if (c == '(') {
-                ss.push(c);
-            } else if (c == ')') {
-                while(!ss.empty() && ss.top() != '(') {
-                    res.push_back(' ');
-                    res.push_back(ss.top());
-                    ss.pop();
-                }
-                ss.pop();
+    int calculate(string s) {
+        int  i = 0;
+        return cal(s, i);
+    }
+
+    int cal(string &s, int &i) {
+        int res = 0;
+        stack<int> ss;
+        int tmp;
+        while (i < s.size()) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                tmp = getNumber(s, i);
+                ss.push(tmp);
+            } else if (s[i] == '(') {
+                ++i;
+                ss.push(cal(s, i));
+            } else if (s[i] == ')') {
+                ++i;
+                break;
             } else {
-                //number
-                res.push_back(c);
+                char tmpOpe = s[i];
+                ++i;
+                tmp = getNumber(s, i);
+                if (tmpOpe == '+') {
+                    ss.push(tmp);
+                } else if (tmpOpe == '-') {
+                    ss.push(tmp * -1);
+                } else if (tmpOpe == '*') {
+                    tmp *= ss.top();
+                    ss.pop();
+                    ss.push(tmp);
+                } else if (tmpOpe == '/') {
+                    tmp = ss.top() / tmp;
+                    ss.pop();
+                    ss.push(tmp);
+                }
             }
         }
-        while(!ss.empty()) {
-            res.push_back(' ');
-            res.push_back(ss.top());
+        while (!ss.empty()) {
+            res += ss.top();
             ss.pop();
         }
-
         return res;
     }
 
     int getNumber(string &s, int &i) {
-        int len = s.size(), res = 0;
-        while (i < len && s[i] >= '0' && s[i] <= '9') {
+        int res = 0;
+        if (s[i] == '(') {
+            ++i;
+            return cal(s, i);
+        }
+        while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
             res *= 10;
             res += s[i] - '0';
             ++i;
         }
         return res;
-    }
-
-    int calculate(string s) {
-        int res = 0;
-        s = infix2suffix(s);
-        cout << s << endl;
-        int len = s.size();
-        stack<int> ss;
-        for (int i = 0; i < len; ++i) {
-            if (s[i] >= '0' && s[i] <= '9') {
-                int tmp = getNumber(s, i);
-                ss.push(tmp);
-            } else if (s[i] == '+') {
-                int tmp1 = ss.top();
-                ss.pop();
-                int tmp2 = ss.top();
-                ss.pop();
-                ss.push(tmp2 + tmp1);
-            } else if (s[i] == '-') {
-                int tmp1 = ss.top();
-                ss.pop();
-                int tmp2 = ss.top();
-                ss.pop();
-                ss.push(tmp2 - tmp1);
-            } else if (s[i] == '*') {
-                int tmp1 = ss.top();
-                ss.pop();
-                int tmp2 = ss.top();
-                ss.pop();
-                ss.push(tmp2 * tmp1);
-            } else if (s[i] == '/') {
-                int tmp1 = ss.top();
-                ss.pop();
-                int tmp2 = ss.top();
-                ss.pop();
-                ss.push(tmp2 / tmp1);
-            }
-        }
-
-        return ss.top();
     }
 
     void check() {
