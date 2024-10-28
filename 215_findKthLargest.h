bd6adaeba73de4e39b84f4c2efb8c24ae499b737
Ke Wang <ke@Kes-MacBook-Pro.attlocal.net>
2024-10-27 22:56:10 -0700

    215 quick select

--- 215_findKthLargest.h
+++ 215_findKthLargest.h
@@ -9,53 +9,36 @@
 
 class FindKthLargest : public solution {
 public:
-    int partition(int left, int right, int pivotIdx, vector<int> &nums) {
-        int pivot = nums[pivotIdx];
+    int quickSelect(int left, int right, int k, vector<int> &nums) {
+        if (left == right) {
+            return left;
+        }
+        // partition
+        int pivot = nums[left];
         int i = left, j = right;
-        while(i < j) {
-            while(i < j && nums[j] <= pivot) --j;
+        while (i < j) {
+            while (i < j && nums[j] <= pivot) --j;
             if (i < j) {
                 nums[i] = nums[j];
-                ++i;
             }
             while (i < j && nums[i] >= pivot) ++i;
             if (i < j) {
                 nums[j] = nums[i];
-                --j;
             }
         }
         nums[i] = pivot;
-        return i;
-    }
 
-    int quickSelect(int left, int right, int k, vector<int> &nums) {
-        if (left == right)
-            return nums[left];
-        int pivotIdx = partition(left, right, left, nums);
-        if (pivotIdx >= k - 1)
-            return quickSelect(left, pivotIdx, k, nums);
-        else
-            return quickSelect(pivotIdx + 1, right, k, nums);
+        if (i == k) {
+            return pivot;
+        } else if (i > k) {
+            return quickSelect(left, i - 1, k, nums);
+        } else {
+            return quickSelect(i + 1, right, k, nums);
+        }
     }
 
     int findKthLargest(vector<int> &nums, int k) {
-//      Nlog(k)
-//        int n = nums.size();
-//        priority_queue<int, vector<int>, greater<int>> qq;
-//        for (int i = 0; i < k; ++i) {
-//            qq.push(nums[i]);
-//        }
-//        for (int i = k; i < n; ++i) {
-//            int tmpT = qq.top();
-//            if (nums[i] > tmpT) {
-//                qq.pop();
-//                qq.push(nums[i]);
-//            }
-//        }
-//        return qq.top();
-
-//      O(N)
-        return quickSelect(0, nums.size() - 1, k, nums);
+        return quickSelect(0, nums.size() - 1, k - 1, nums);
     }
 
     void check() {
