cea463f027845056ede5ad1e3e51a4d5612f105a
Ke Wang <ke@Kes-MacBook-Pro.attlocal.net>
2024-10-30 20:44:19 -0700

    4

--- 4_findMedianSortedArrays.h
+++ 4_findMedianSortedArrays.h
@@ -9,36 +9,41 @@
 
 class FindMedianSortedArrays : public solution {
 public:
-    double kth(vector<int> &nums1, vector<int> &nums2, int l1, int l2, int k) {
-        int len1 = nums1.size() - l1, len2 = nums2.size() - l2;
-        if (len1 < len2)
-            return kth(nums2, nums1, l2, l1, k);
-        if (len2 == 0)
+    int findKthNum(int k, int l1, int l2, vector<int> &nums1, vector<int> &nums2) {
+        int n = nums1.size() - l1, m = nums2.size() - l2;
+        if (n < m) {
+            return findKthNum(k, l2, l1, nums2, nums1);
+        }
+        if (m == 0) {
             return nums1[l1 + k - 1];
-        if (k == 1)
+        }
+        if (k == 1) {
             return min(nums1[l1], nums2[l2]);
-        int m2 = min(len2, k / 2), m1 = k - m2;
-        if (nums1[l1 + m1 - 1] > nums2[l2 + m2 - 1]) {
-            return kth(nums1, nums2, l1, l2 + m2, k - m2);
+        }
+        int midIdx2 = min(k / 2 - 1, m - 1), midIdx1 = k - midIdx2 - 2;
+        int mid1 = nums1[l1 + midIdx1], mid2 = nums2[l2 + midIdx2];
+        if (mid1 == mid2) {
+            return mid1;
+        } else if (mid1 < mid2) {
+            return findKthNum(k - midIdx1 - 1, l1 + midIdx1 + 1, l2, nums1, nums2);
         } else {
-            return kth(nums1, nums2, l1 + m1, l2, k - m1);
+            return findKthNum(k - midIdx2 - 1, l1, l2 + midIdx2 + 1, nums1, nums2);
         }
     }
 
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
         double res = 0;
-        int len1 = nums1.size(), len2 = nums2.size(), k = (len1 + len2 + 1) / 2;
-        res = kth(nums1, nums2, 0, 0, k);
-        if ((len1 + len2) % 2 == 0) {
-            res += kth(nums1, nums2, 0, 0, k + 1);
-            res /= 2;
+        int n = nums1.size(), m = nums2.size();
+        if ((n + m) % 2 == 1) {
+            res = findKthNum((n + m + 1) / 2, 0, 0, nums1, nums2);
+        } else {
+            res = ((double) findKthNum((n + m) / 2, 0, 0, nums1, nums2) + findKthNum((n + m) / 2 + 1, 0, 0, nums1, nums2)) / 2;
         }
-
         return res;
     }
 
     void check() {
-        vector<int> nums1{1}, nums2{2, 3, 4};
+        vector<int> nums1{1,2,3,4,5}, nums2{6,7,8,9,10,11,12,13,14,15,16,17};
         double res = findMedianSortedArrays(nums1, nums2);
         cout << res << endl;
     }
