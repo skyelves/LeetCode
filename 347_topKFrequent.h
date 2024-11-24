1907b690506dfd9d2de5c20dc31638da4d2d960a
Ke Wang <ke@Kes-MacBook-Pro.attlocal.net>
2024-11-23 22:16:10 -0800

    347 topk frequent O(n)

--- 347_topKFrequent.h
+++ 347_topKFrequent.h
@@ -8,22 +8,31 @@
 #include "solution.h"
 
 class TopKFrequent : public solution {
+    struct cmp {
+        bool operator() (const pair<int, int> &a, const pair<int, int> &b) {
+            return a.second > b.second;
+        }
+    };
+
 public:
     vector<int> topKFrequent(vector<int> &nums, int k) {
-        vector<int> res;
-        map<int, int> mm;
+        vector<int> res(k);
+        unordered_map<int, int> mm;
         for (auto i : nums) {
             ++mm[i];
         }
-        vector<pair<int, int>> toSort;
-        for (auto i : mm) {
-            toSort.push_back(i);
+        vector<vector<int>> buckets(nums.size() + 1, vector<int>());
+        for (auto &tmp : mm) {
+            buckets[tmp.second].push_back(tmp.first);
         }
-        sort(toSort.begin(), toSort.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
-            return a.second > b.second;
-        });
-        for (int i = 0; i < k; ++i) {
-            res.push_back(toSort[i].first);
+        int cnt = 0;
+        for (int i = nums.size(); i > 0 && cnt < k; --i) {
+            if (!buckets[i].empty()) {
+                for (int j = 0; j < buckets[i].size() && cnt < k; ++j) {
+                    res[cnt] = buckets[i][j];
+                    ++cnt;
+                }
+            }
         }
 
         return res;
